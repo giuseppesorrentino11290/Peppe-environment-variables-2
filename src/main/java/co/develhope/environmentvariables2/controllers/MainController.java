c5c8cb7a37e1ab3d79bd9d6afc03211ba1030200
giuseppe sorrentino <giuseppesorrentino11290@gmail.com>
2023-03-01 15:52:21 +0100

    environment 2

---
+++ src/main/java/co/develhope/environmentvariables2/controllers/MainController.java
@@ -0,0 +1,21 @@
+package co.develhope.environmentvariables2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/message")
+    public String message(){
+
+        return environment.getProperty("myCustomEnvs.welcomeMsg");
+    }
+}
